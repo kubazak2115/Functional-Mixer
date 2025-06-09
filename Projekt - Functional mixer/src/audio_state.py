dc1ae20ee211e9178f4c7a46d4fad3d3e9812324
Nside <bochenek@student.agh.edu.pl>
2025-06-09 16:46:47 +0200

    Final wersja audiostate'a
    zmiany w układzie aplikajci cześć audiostate i audioprocessingu przerzucam do maina

--- Projekt - Functional mixer/src/audio_state.py
+++ Projekt - Functional mixer/src/audio_state.py
@@ -1,19 +1,12 @@
-import tkinter as tk
-import threading
-from typing import Dict, Any
+from typing import Dict, Any, List
 
 class AudioState:
-    def __init__(self):
-        self.state_lock = threading.RLock()
-        self.root = None
-        self.gui = None
-        self.waveform_display = None
-        self.crossfader_var = tk.DoubleVar(value=50)
-        self.volume_vars = [tk.DoubleVar(value=70) for _ in range(2)]
-        self._initialize_state()
+    def init(self):
+        self.state = self._initialize_audio_state()
+        self.state_lock = None  # Will be set by main application
 
-    def _initialize_state(self) -> Dict[str, Any]:
-        self._state = {
+    def _initialize_audio_state(self) -> Dict[str, Any]:
+        return {
             'files': [None, None],
             'filenames': [None, None],
             'data': [None, None],
@@ -29,13 +22,6 @@ class AudioState:
             'beat_times': [[], []],
             'tempo_confidence': [0.0, 0.0],
             'waveform_cache': [None, None],
-            'bpm_analyzing': [False, False]
+            'bpm_analyzing': [False, False],
+            'time_axes': [None, None]
         }
-
-    def __getitem__(self, key):
-        with self.state_lock:
-            return self._state[key]
-
-    def __setitem__(self, key, value):
-        with self.state_lock:
-            self._state[key] = value
