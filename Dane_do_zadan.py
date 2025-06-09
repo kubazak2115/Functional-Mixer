b9f1c21c950c1f6750f5f3da8ef485d695774bf6
Nside <bochenek@student.agh.edu.pl>
2025-06-09 19:19:37 +0200

    dodaje dane do zadań

---
+++ Dane_do_zadan.py
@@ -0,0 +1,21 @@
+# Zadanie 1
+
+users = ["Ala", "Ola", "Jan", "Ela", "Kot"]
+scores = [75, 45, 90, 60, 50]
+
+# Zadanie 2
+
+students = ["Ania", "Bartek", "Celina", "Darek", "Ewa"]
+grades = [4.5, 2.0, 3.0, 5.0, 3.5]
+
+# zadanie 3
+
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+# zadanie 5
+
+text = "omg JPWP jest super"
